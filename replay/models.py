0048576e0c53cc71038192d4b57e3903cc9be66e
Grant Jenks <contact@grantjenks.com>
2017-02-23 12:39:57 -0800

    Add help text to model fields

--- replay/models.py
+++ replay/models.py
@@ -15,19 +15,32 @@ HTTP_METHODS = (
 
 class Scenario(models.Model):
     name = models.TextField(unique=True)
-    priority = models.FloatField(default=1.0)
+    priority = models.FloatField(
+        default=1.0,
+        help_text='Scenarios are run in ascending priority.',
+    )
 
     def __str__(self):
         return self.name
 
 class Action(models.Model):
     scenario = models.ForeignKey(Scenario, null=True, blank=True, default=None)
-    order = models.FloatField(default=1.0)
+    order = models.FloatField(
+        default=1.0,
+        help_text='Actions are run in ascending order.',
+    )
     name = models.TextField(blank=True, default='')
     method = models.CharField(max_length=8, choices=HTTP_METHODS)
     path = models.TextField()
-    data = models.TextField()
-    files = models.TextField()
+    data = models.TextField(
+        help_text='HTTP parameters in JSON format. Supports string templating.'
+        ' Example: $name or $(name).',
+    )
+    files = models.TextField(
+        help_text='Uploaded files in JSON format. Key and value pairs'
+        ' correspond to filename and file-path pairs. Supports string'
+        ' templating. Example: $name and $(name).',
+    )
     status_code = models.CharField(max_length=3)
     content = models.TextField(blank=True)
 
@@ -38,8 +51,15 @@ class Action(models.Model):
 
 class Validator(models.Model):
     action = models.ForeignKey(Action, related_name='validators')
-    order = models.FloatField(default=1.0)
-    pattern = models.TextField()
+    order = models.FloatField(
+        default=1.0,
+        help_text='Validators are run in ascending order.',
+    )
+    pattern = models.TextField(
+        help_text='Matched against HTTP response. Supports regular'
+        ' expressions. Symbolic group names are stored and may be'
+        ' used for string templating. Example: (?P&lt;name&gt;pattern)',
+    )
 
     def __str__(self):
         return self.pattern[:68]
