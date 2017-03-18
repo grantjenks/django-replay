abed62b17ba782cf8e4e8799063a9e00698f33ec
Grant Jenks <contact@grantjenks.com>
2017-03-17 17:10:54 -0700

    Bump version to 0.5.1

--- replay/__init__.py
+++ replay/__init__.py
@@ -3,8 +3,8 @@
 """
 
 __title__ = 'replay'
-__version__ = '0.5.0'
-__build__ = 0x000500
+__version__ = '0.5.1'
+__build__ = 0x000501
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2017 Grant Jenks'
