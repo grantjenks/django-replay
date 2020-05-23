27921978aeca3ff8a8071bb3ba9b1240fae229f5
Grant Jenks <grant.jenks@gmail.com>
2020-05-22 23:18:30 -0700

    Bump version to 0.9.1

--- replay/__init__.py
+++ replay/__init__.py
@@ -3,8 +3,8 @@
 """
 
 __title__ = 'replay'
-__version__ = '0.9.0'
-__build__ = 0x000900
+__version__ = '0.9.1'
+__build__ = 0x000901
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2017 Grant Jenks'
