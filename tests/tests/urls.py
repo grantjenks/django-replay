1862ae2a642743e909d786f94c90f765bc5b3803
Grant Jenks <grant.jenks@gmail.com>
2022-06-18 23:32:48 -0700

    Update urls for Django upgrade

--- tests/tests/urls.py
+++ tests/tests/urls.py
@@ -1,24 +1,25 @@
-"""tests URL Configuration
+"""www URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 
-from tests import views
+from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^essays/(?P<essay_id>\d+)', views.essay, name='essay'),
-    url(r'^admin/', include(admin.site.urls)),
+    path('/', views.index, name='index'),
+    path('essays/<int:essay_id>', views.essay, name='essay'),
+    path('admin/', admin.site.urls),
 ]
