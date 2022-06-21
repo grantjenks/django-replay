fc0ebbd0765db554ab6e5e12b5089014f7490527
Grant Jenks <grant.jenks@gmail.com>
2022-06-20 22:43:55 -0700

    Test all scenarios with fixture

--- tests/www/tests.py
+++ tests/www/tests.py
@@ -1,12 +1,16 @@
+import pathlib
+import pytest
+
+from django.contrib.auth.models import User
 from django.test import TestCase
-from replay.utils import test_scenarios, test_scenario
+from replay.utils import test_scenarios
+
+pytestmark = pytest.mark.django_db
 
 
 class ReplayTestCase(TestCase):
-    fixtures = ['replay.json']
+    fixtures = [pathlib.Path(__file__).parent.parent / 'replay.json']
 
     def test_scenarios(self):
+        User.objects.create_superuser(username='admin', password='password')
         test_scenarios()
-
-    def test_create_essay(self):
-        test_scenario(name='Create Essay')
