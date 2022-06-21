756b668183a76c4466d2bdad4f0708b1382b6207
Grant Jenks <grant.jenks@gmail.com>
2022-06-20 22:43:13 -0700

    Thread test client through api

--- replay/utils.py
+++ replay/utils.py
@@ -1,17 +1,23 @@
+from django.test import Client
+
 from replay.models import Scenario
 
 
-def test_scenarios(state=None, **kwargs):
+def test_scenarios(client=None, state=None, **kwargs):
+    if client is None:
+        client = Client()
     if state is None:
         state = {}
     scenarios = Scenario.objects.filter(**kwargs).order_by('priority', 'id')
     scenario_ids = scenarios.values_list('id', flat=True)
     for scenario_id in scenario_ids:
-        test_scenario(state=state, pk=scenario_id)
+        test_scenario(client=client, state=state, pk=scenario_id)
 
 
-def test_scenario(state=None, **kwargs):
+def test_scenario(client=None, state=None, **kwargs):
+    if client is None:
+        client = Client()
     if state is None:
         state = {}
     scenario = Scenario.objects.get(**kwargs)
-    scenario.test(state)
+    scenario.test(client=client, state=state)
