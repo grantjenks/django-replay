25ac55389b041f08a7043c04772ab94617f0629c
Grant Jenks <grant.jenks@gmail.com>
2022-06-20 22:42:24 -0700

    Monkey-patch allowed hosts and support verbose logging

--- replay/management/commands/replaytest.py
+++ replay/management/commands/replaytest.py
@@ -1,16 +1,33 @@
 """Test Scenario Management Command
 """
 
+import logging
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from ...utils import test_scenario, test_scenarios
 
 
 class Command(BaseCommand):
+    LOG_LEVELS = {
+        0: logging.ERROR,
+        1: logging.WARNING,
+        2: logging.INFO,
+        3: logging.DEBUG,
+    }
+
     def add_arguments(self, parser):
         parser.add_argument('scenario', nargs='*')
 
     def handle(self, *args, **options):
+        settings.ALLOWED_HOSTS.append('testserver')
+        verbosity = options['verbosity']
+        log_level = self.LOG_LEVELS[verbosity]
+        logging.basicConfig(
+            format='%(asctime)s %(levelname)8s: %(message)s',
+            level=log_level,
+        )
         names = options['scenario']
         if names:
             for name in names:
