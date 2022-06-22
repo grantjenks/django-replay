00e27ffb2d55f2e09708c0668fede2e1d229e753
Grant Jenks <grant.jenks@gmail.com>
2022-06-21 20:38:23 -0700

    Add tests for replay command

--- tests/www/tests.py
+++ tests/www/tests.py
@@ -2,6 +2,7 @@ import pathlib
 
 import pytest
 from django.contrib.auth.models import User
+from django.core.management import call_command
 from django.test import TestCase
 
 from replay.utils import test_scenarios
@@ -15,3 +16,11 @@ class ReplayTestCase(TestCase):
     def test_scenarios(self):
         User.objects.create_superuser(username='admin', password='password')
         test_scenarios()
+
+    def test_replaytest(self):
+        User.objects.create_superuser(username='admin', password='password')
+        call_command('replaytest')
+
+    def test_replaytest_scenario(self):
+        User.objects.create_superuser(username='admin', password='password')
+        call_command('replaytest', 'Admin Login')
