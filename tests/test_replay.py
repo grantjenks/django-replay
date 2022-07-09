e7769204a3f0f6d669c14d66579ad25dae0c2351
Grant Jenks <grant.jenks@gmail.com>
2022-07-08 21:23:15 -0700

    Test replayrecord command

--- tests/test_replay.py
+++ tests/test_replay.py
@@ -1,5 +1,9 @@
 import pytest
 
+from unittest import mock
+
+from django.core.management import call_command
+
 from replay.models import Action
 
 pytestmark = pytest.mark.django_db
@@ -16,3 +20,13 @@ def test_action():
         content='',
     )
     assert str(action) == 'Test'
+
+
+def test_record(monkeypatch):
+    mock_call_command = mock.Mock()
+    monkeypatch.setattr(
+        'replay.management.commands.replayrecord.call_command',
+        mock_call_command,
+    )
+    call_command('replayrecord')
+    mock_call_command.assert_called()
